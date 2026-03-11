c2c2853fdfa4089311a0a7d6ffa6aec0fc9888c0
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2026-03-11 21:23:18 +0900

    [level 0] Title: 글자 지우기, Time: 0.06 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        boolean[] remove = new boolean[my_string.length()];
+        
+        for(int idx : indices){
+            remove[idx] = true;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(!remove[i]){
+                answer.append(my_string.charAt(i));
+            }
+        }
+        
+        return answer.toString();
+    }
+}
